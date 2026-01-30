aa476d6edc4b12c1a29d167da8ce155f81f9b0bb
Victor Didic <75629328+DidicV@users.noreply.github.com>
2026-01-30 13:42:50 +0200

    Activity 3

--- Data/EventStore.cs
+++ Data/EventStore.cs
@@ -8,6 +8,12 @@ namespace CourseraBlazor.Data
         {
             new Event { Id = 1, Name = "Corporate Summit", Date = DateTime.Today.AddDays(10), Location = "London" },
             new Event { Id = 2, Name = "Tech Conference", Date = DateTime.Today.AddDays(20), Location = "Berlin" }
-        };  
+        };
+
+        public void AddEvent(Event newEvent)
+        {
+            newEvent.Id = Events.Count + 1;
+            Events.Add(newEvent);
+        }
     }
 }
