90cd848d8a0f723004f7df49e520562aef17cc27
Victor Didic <75629328+DidicV@users.noreply.github.com>
2026-01-30 13:15:30 +0200

    Initial commit

---
+++ Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseraBlazor.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Location { get; set; } = string.Empty;
+    }
+}
